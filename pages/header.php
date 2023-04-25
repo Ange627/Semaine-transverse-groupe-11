2dad7f34af265ae44dde614dbe54dbff83b013b7
Ange627 <arnold_du_67@hotmail.fr>
2023-04-25 11:39:35 +0200

    création header

---
+++ pages/header.php
@@ -0,0 +1,17 @@
+
+<?php
+    $servername = "localhost"; // le nom du serveur de base de données
+    $username = "roo"; // le nom d'utilisateur pour se connecter à la base de données
+    $password = ""; // le mot de passe pour se connecter à la base de données
+    $dbname = "bdd-diagnostic  "; // le nom de la base de données à laquelle se connecter
+
+    // création de la connexion
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // vérification de la connexion
+    if (!$conn) {
+        die("La connexion a échoué : " . mysqli_connect_error());
+    }
+
+    echo "Connexion réussie";
+?>
