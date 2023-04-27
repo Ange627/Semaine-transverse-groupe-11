21a51404bef851584b7b31d8bae74f55f97e7812
Ange627 <arnold_du_67@hotmail.fr>
2023-04-27 17:07:19 +0200

    radar

--- pages/home.php
+++ pages/home.php
@@ -30,6 +30,57 @@
     <p>Axe Réactivité 3.3/5</p>
     <p>Axe Compétences 2.9/5</p>
     <p>Axe Numérique 3.6/5</p>
+
+    <?php
+// Récupération des scores de la table reactivité
+$requete_reactivite = "SELECT score FROM reactivite WHERE identreprise='$id'";
+$resultat_reactivite = mysqli_query($conn, $requete_reactivite);
+$scores_reactivite = mysqli_fetch_all($resultat_reactivite, MYSQLI_ASSOC);
+
+// Récupération des scores de la table numérique
+$requete_numerique = "SELECT score FROM numerique WHERE identreprise='$id'";
+$resultat_numerique = mysqli_query($conn, $requete_numerique);
+$scores_numerique = mysqli_fetch_all($resultat_numerique, MYSQLI_ASSOC);
+
+// Récupération des scores de la table compétence
+$requete_competence = "SELECT score FROM competence WHERE identreprise='$id'";
+$resultat_competence = mysqli_query($conn, $requete_competence);
+$scores_competence = mysqli_fetch_all($resultat_competence, MYSQLI_ASSOC);
+
+// Transformation des scores en pourcentages sur 5
+$max_score = 5;
+$score_reactivite = round(array_sum(array_column($scores_reactivite, 'score')) / count($scores_reactivite) * $max_score);
+$score_numerique = round(array_sum(array_column($scores_numerique, 'score')) / count($scores_numerique) * $max_score);
+$score_competence = round(array_sum(array_column($scores_competence, 'score')) / count($scores_competence) * $max_score);
+
+// Affichage du graphique radar
+echo "<canvas id='radarChart'></canvas>";
+echo "<script src='https://cdn.jsdelivr.net/npm/chart.js'></script>";
+echo "<script>
+        var radarChart = new Chart(document.getElementById('radarChart'), {
+            type: 'radar',
+            data: {
+                labels: ['Réactivité', 'Numérique', 'Compétence'],
+                datasets: [{
+                    label: 'Score',
+                    data: [$score_reactivite, $score_numerique, $score_competence],
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scale: {
+                    ticks: {
+                        beginAtZero: true,
+                        max: 3
+                    }
+                }
+            }
+        });
+      </script>";
+?>
+
   
 
 
